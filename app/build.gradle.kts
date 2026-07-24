0a205d2ac55ec4d6d56d07f2f9bcd648bbdb2327
Evgenii Vedenin <ea.galda@gmail.com>
2026-07-24 10:42:32 +0300

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
     defaultConfig {
         minSdk = 30
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.0.5"
+        versionCode = 15
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
